855a519eb45aae66ddadce6d9ff44c3222284ba2
andrii663 <andriikosenko663@gmail.com>
2024-08-18 06:13:57 -0400

    enhance watcher.

--- app/watcher.py
+++ app/watcher.py
@@ -15,96 +15,96 @@ def cv2_to_pil(cv2_image):
     """  
     return Image.fromarray(cv2.cvtColor(cv2_image, cv2.COLOR_BGR2RGB))  
 
-def watch_video(video_path):  
-    # Open a connection to the video file  
-    cap = cv2.VideoCapture(video_path)  # Use the video file path  
+# def watch_video(video_path):  
+#     # Open a connection to the video file  
+#     cap = cv2.VideoCapture(video_path)  # Use the video file path  
 
-    logging.info("Watching video...")
-    if not cap.isOpened():  
-        print(f"Error: Could not open video file: {video_path}")  
-        return  None
-    print(video_path)
-    mod = 1
-    is_parcel_exist = None
-    frame_index = 0  
-    count = 0
-    while True:  
-        # Read a frame from the video  
-        ret, frame = cap.read()  
-        if not ret:  
-            print("Reached end of video file or encountered an error")  
-            break  
-        print(f"Processing frame {frame_index}")  
-        frame_index += 1  
+#     logging.info("Watching video...")
+#     if not cap.isOpened():  
+#         print(f"Error: Could not open video file: {video_path}")  
+#         return  None
+#     print(video_path)
+#     mod = 1
+#     is_parcel_exist = None
+#     frame_index = 0  
+#     count = 0
+#     while True:  
+#         # Read a frame from the video  
+#         ret, frame = cap.read()  
+#         if not ret:  
+#             print("Reached end of video file or encountered an error")  
+#             break  
+#         print(f"Processing frame {frame_index}")  
+#         frame_index += 1  
 
-        # Convert the OpenCV frame to a Pillow image  
-        pil_image = cv2_to_pil(frame)  
+#         # Convert the OpenCV frame to a Pillow image  
+#         pil_image = cv2_to_pil(frame)  
 
-        # if frame_index>130:
-        #     pil_image.show()
-        result = detect_parcel(pil_image)  
-        # if result is None:  
-        #     # print("detect_parcel returned None")  
-        #     continue  # handle this case appropriately 
-        img_with_box, is_parcel_exist = result
-        # Detect parcel in the Pillow image  
-        print(is_parcel_exist)
-        # print(is_parcel_exist)
-        if is_parcel_exist == None:  
-            count += 1
-            if count>24:
-                mod = 0  
-                break
-        else:
-            # img_with_box.show()
-            count = 0
-        if frame_index>48:
-            mod = 1
-            break
-        # Get the current date and time in the specified format  
-        # current_time = datetime.now().strftime(date_format)  
-        # current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  
+#         # if frame_index>130:
+#         #     pil_image.show()
+#         result = detect_parcel(pil_image)  
+#         # if result is None:  
+#         #     # print("detect_parcel returned None")  
+#         #     continue  # handle this case appropriately 
+#         img_with_box, is_parcel_exist = result
+#         # Detect parcel in the Pillow image  
+#         print(is_parcel_exist)
+#         # print(is_parcel_exist)
+#         if is_parcel_exist == None:  
+#             count += 1
+#             if count>24:
+#                 mod = 0  
+#                 break
+#         else:
+#             # img_with_box.show()
+#             count = 0
+#         if frame_index>48:
+#             mod = 1
+#             break
+#         # Get the current date and time in the specified format  
+#         # current_time = datetime.now().strftime(date_format)  
+#         # current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  
 
-        # Print the current mode and timestamp to the console (for debugging)  
-        # print(f'{current_time} - Mode: {mod}')  
+#         # Print the current mode and timestamp to the console (for debugging)  
+#         # print(f'{current_time} - Mode: {mod}')  
         
-        # # Show the frame 
-        # if frame_index>200:
-        # cv2.imshow('Video', frame)  
+#         # # Show the frame 
+#         # if frame_index>200:
+#         # cv2.imshow('Video', frame)  
 
-        # Break the loop if 'q' is pressed  
-        if cv2.waitKey(1) & 0xFF == ord('q'):  
-            break  
-    cap.release()  
-    cv2.destroyAllWindows()  
-    return mod
+#         # Break the loop if 'q' is pressed  
+#         if cv2.waitKey(1) & 0xFF == ord('q'):  
+#             break  
+#     cap.release()  
+#     cv2.destroyAllWindows()  
+#     return mod
 
-def watcher(date_format):
-    '''
-    search video clip  whether the parcel is taken.
-    '''
-    subfolder_num = int(date_format[11:13]) - 10 if int(date_format[11:13]) - 10 >= 0 else int(date_format[11:13]) + 14  
-    subfolder_num_str = str(subfolder_num).zfill(2)  
+# def watcher(date_format):
+#     '''
+#     search video clip  whether the parcel is taken.
+#     '''
+#     subfolder_num = int(date_format[11:13]) - 10 if int(date_format[11:13]) - 10 >= 0 else int(date_format[11:13]) + 14  
+#     subfolder_num_str = str(subfolder_num).zfill(2)  
     
-    logging.info("Watcher script is running.")
+#     logging.info("Watcher script is running.")
 
-    if int(date_format[11:13]) - 10 < 0:
-        # date_format[8:10] = str(int(date_format[8:10])-1).zfill(2)
-        date_format = date_format[:8] + str(int(date_format[8:10]) - 1).zfill(2) + date_format[10:]
+#     if int(date_format[11:13]) - 10 < 0:
+#         # date_format[8:10] = str(int(date_format[8:10])-1).zfill(2)
+#         date_format = date_format[:8] + str(int(date_format[8:10]) - 1).zfill(2) + date_format[10:]
 
-    video_folder = f'{constants.RECORDINGS_DIR}/{date_format[:10]}/{subfolder_num_str}/{constants.CAMERA_NAME}/'  
+#     video_folder = f'{constants.RECORDINGS_DIR}/{date_format[:10]}/{subfolder_num_str}/{constants.CAMERA_NAME}/'  
 
-    # Call the function to get matching files  
-    matching_files = find_video_path(video_folder, date_format)  
+#     # Call the function to get matching files  
+#     matching_files = find_video_path(video_folder, date_format)  
 
-    mod = 1
-    # Print matching file paths  
-    for file_path in matching_files:  
-       mod = watch_video(file_path)
-       break
-    return mod
+#     mod = 1
+#     # Print matching file paths  
+#     for file_path in matching_files:  
+#        mod = watch_video(file_path)
+#        break
+#     return mod
 
-def find_video_path(video_folder, date_format):
+# def find_video_path(video_folder, date_format):
 
     try:
         #Find the video in the folder
@@ -158,3 +158,138 @@ def find_video_path(video_folder, date_format):
     except Exception as e:  
         print(f"An error occurred: {e}")  
         return []  
+
+
+def watch_video(video_path):  
+    cap = cv2.VideoCapture(video_path)  # Open a connection to the video file  
+
+    logging.info("Watching video...")  
+    if not cap.isOpened():  
+        logging.error(f"Error: Could not open video file: {video_path}")  
+        return None  
+
+    frame_index = 0  
+    count = 0  
+    mod = 1  
+    is_parcel_exist = None  
+
+    while True:  
+        try:  
+            ret, frame = cap.read()  # Read a frame from the video  
+            if not ret:  
+                logging.info("Reached end of video file or encountered an error")  
+                break  
+
+            logging.info(f"Processing frame {frame_index}")  
+            frame_index += 1  
+
+            if frame is None or frame.size == 0:  
+                logging.error("Empty or invalid frame encountered")  
+                continue  
+
+            pil_image = cv2_to_pil(frame)  # Convert the OpenCV frame to a Pillow image  
+
+            result = detect_parcel(pil_image)  
+            if result is None:  
+                count += 1  
+                if count > 24:  
+                    mod = 0  
+                    break  
+            else:  
+                img_with_box, is_parcel_exist = result  
+                logging.info(f"Parcel detection result: {is_parcel_exist}")  
+                count = 0  
+
+            if frame_index > 48:  
+                mod = 1  
+                break  
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):  
+                break  
+
+        except Exception as e:  
+            logging.error(f"Error processing video frame: {e}")  
+            break  
+
+    cap.release()  
+    cv2.destroyAllWindows()  
+    return mod  
+
+
+def watcher(date_format):  
+    '''  
+    search video clip  whether the parcel is taken.  
+    '''  
+    subfolder_num = int(date_format[11:13]) - 10 if int(date_format[11:13]) - 10 >= 0 else int(date_format[11:13]) + 14  
+    subfolder_num_str = str(subfolder_num).zfill(2)  
+    
+    logging.info("Watcher script is running.")  
+
+    if int(date_format[11:13]) - 10 < 0:  
+        # date_format[8:10] = str(int(date_format[8:10])-1).zfill(2)  
+        date_format = date_format[:8] + str(int(date_format[8:10]) - 1).zfill(2) + date_format[10:]  
+
+    video_folder = f'{constants.RECORDINGS_DIR}/{date_format[:10]}/{subfolder_num_str}/{constants.CAMERA_NAME}/'  
+
+    # Call the function to get matching files  
+    matching_files = find_video_path(video_folder, date_format)  
+
+    mod = 1  
+    # Print matching file paths  
+    for file_path in matching_files:  
+       mod = watch_video(file_path)  
+       break  
+    return mod  
+
+
+def find_video_path(video_folder, date_format):  
+    try:  
+        logging.info("Finding the video...")  
+        
+        prefix = date_format[14:16]  
+        prefix_opt = int(date_format[14:16]) - 1  
+        prefix_opt = f"{prefix_opt:02d}"   
+
+        max_number = int(date_format[17:19])  
+        if max_number >= 10:  
+            min_number = max_number - 10  
+        else:    
+            min_number = max_number + 50  
+
+        logging.info(f"prefix:{prefix}")  
+
+        time.sleep(10)  # Consider removing or reducing sleep in production  
+        
+        files = os.listdir(video_folder)  # List all files in the directory  
+        logging.info(f"Files in folder: {files}")  
+        
+        matching_files = []  # Filter files based on specified conditions  
+        for file in files:  
+            if file.startswith(prefix) and max_number >= 10:  
+                logging.info("Prefix match found")  
+                parts = file.split('.')  
+                logging.info(f"File parts: {parts}")  
+
+                if len(parts) == 3 and parts[0].isdigit() and parts[1].isdigit():  
+                    second_num = int(parts[1])  
+                    if min_number <= second_num < max_number:  
+                        matching_files.append(os.path.join(video_folder, file))   
+            elif file.startswith(prefix_opt) and max_number < 10:  
+                logging.info("Optional prefix match found")  
+                parts = file.split('.')  
+                logging.info(f"File parts: {parts}")  
+
+                if len(parts) == 3 and parts[0].isdigit() and parts[1].isdigit():  
+                    second_num = int(parts[1])  
+                    if min_number < second_num:  
+                        matching_files.append(os.path.join(video_folder, file))   
+
+        logging.info(f"Matching filenames: {matching_files}")  
+        return matching_files  
+
+    except Exception as e:  
+        logging.error(f"An error occurred: {e}")  
+        return []  
+
+# Initialize logging  
+logging.basicConfig(level=logging.INFO)  
